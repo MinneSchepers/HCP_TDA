18da9c85e6987ab9dfcc95d45e4d5e9db409c08a
MinneSchepers <minneschepers@gmail.com>
2022-02-25 13:32:40 +0100

    Create Python_Template_Header_Instructions.py

---
+++ Python_Template_Header_Instructions.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""A short description or title.
+
+   Longer description.
+
+"""
+
+__author__ = "Name"
+__contact__ = "Email"
+__date__ = "YYYY/MM/DD"   ### Date it was created
+__status__ = "Production" ### Production = still being developed. Else: Concluded/Finished.
+
+
+####################
+# Review History   #
+####################
+
+# Reviewed by Name Date ### e.g. Eduarda Centeno 20200909
+
+
+####################
+# Libraries        #
+####################
+
+# Standard imports  ### (Put here built-in libraries - https://docs.python.org/3/library/)
+
+
+# Third party imports ### (Put here third-party libraries e.g. pandas, numpy)
+import numpy as np # version 1.0.1
+
+
+# Internal imports ### (Put here imports that are related to internal codes from the lab)
+
+
+##### Please remove any patient/subject identifier from the code
+##### Please use English (i.e. replace comments in Dutch or other languages)
+##### Please include packages-libraries versions with a hashtag next to each of them (as numpy example above).
+##### Please consider leaving a pip freeze requirements file in your projects folder so that we can keep track of all package versions used at the time the project was being executed. (https://blog.usejournal.com/why-and-how-to-make-a-requirements-txt-f329c685181e)
+##### The comments in this file (e.g. ### Put here built-in..) do not need to be propagated to your code. They are only to give instructions in this template file.
+##### Please consider using sections divisions such as the ones above enclosed by hashtags. 
+##### Please also have a look at https://www.python.org/dev/peps/pep-0008/. This will explain how to follow the Python community standards. E.g. i=i+1 is wrong, should be i = i + 1
+##### Please consider limiting all lines to 72 characters (instructions on how to do that correctly can be found in PEP-0008 Maximum Line Length section).
+##### Have a look at black.vercel.app (this is a browser link). This app will help you quickly format you code (basic formatting). Then you can proceed with manual enrichment.
+##### Please document your functions to create docstrings (https://www.python.org/dev/peps/pep-0257/). Example:
+
+def function(args):
+    """Short description.
+    
+    Parameters
+    ----------
+    Argument1: what it should be, which format, example of file (if available)
+    
+    
+    Examples
+    ----------
+    An example of how to use it, if necessary.
+
+    
+    Returns
+    -------
+    out: what is the output, in which format. 
+
+
+    Notes
+    ----------
+    Details, if relevant. For example, function ignores NaNs or not. 
+
+    """
+
